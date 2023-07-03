5828ef91191540dd13841022a75d295025fa006c
Ahmed Abdo <devahmedabdo@gmail.com>
2023-07-03 19:46:54 +0300

    first customers page part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -148,15 +148,13 @@ export class AppComponent implements OnInit {
     instagramProfile: null,
   };
   videosSlides: any[] = [
-    // 'https://www.youtube.com/embed/jMW8CtkaPgI',
-    // 'https://www.youtube.com/embed/jCTxWpYHIbQ',eLxRAj-bD0o
-    'https://www.youtube.com/embed/eLxRAj-bD0o',
-    'https://www.youtube.com/embed/4abvqPpUcxw',
-    // 'https://www.youtube.com/embed/1WWmR3zC29g',
-
-    'https://www.youtube.com/embed/TGZkt-3Ujn0',
     'https://www.youtube.com/embed/BhyMMzMosL4',
+    'https://www.youtube.com/embed/TGZkt-3Ujn0',
     'https://www.youtube.com/embed/lSQTgeDvOlA',
+    'https://www.youtube.com/embed/4abvqPpUcxw',
+    'https://www.youtube.com/embed/eLxRAj-bD0o',
+
+    //
   ];
   increasStep() {
     if (
